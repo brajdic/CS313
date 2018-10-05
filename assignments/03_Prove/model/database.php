a1933c770ef817562d38f20e1d0d4b283ed40676
brajdic <32497664+brajdic@users.noreply.github.com>
2018-10-05 17:09:51 -0400

    fixed?

--- assignments/03_Prove/model/database.php
+++ assignments/03_Prove/model/database.php
@@ -1,5 +1,5 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=Ashton';
+    $dsn = 'mysql:host=172.245.60.70;dbname=Ashton';
     $username = 'root';
     $password = 'pizza';
 
@@ -7,7 +7,7 @@
         $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
         $error_message = $e->getMessage();
-        //include('../errors/database_error.php');
+        echo("Nope");
         exit();
     }
 ?>
