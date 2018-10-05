7c0fcabc4da02fbb0a90a40444a955957a2aba40
brajdic <32497664+brajdic@users.noreply.github.com>
2018-10-05 17:47:45 -0400

    Working?

--- assignments/03_Prove/model/database.php
+++ assignments/03_Prove/model/database.php
@@ -1,5 +1,5 @@
 <?php
-    $dsn = 'pgsql:dbname=Ashton;port=5432;host=172.245.60.70';
+    $dsn = 'mysql:dbname=Ashton;port=5432;host=172.245.60.70';
     $username = 'root';
     $password = 'pizza';
 
